8d3adeae7e469c761071304d6258c4fa5fd7ec1e
Nahuel <117694206+Nahupenayo@users.noreply.github.com>
2025-06-01 19:40:59 -0300

    Subo trabajo

---
+++ Calculadora_IMC/src/main/java/logica/Calculadora_IMC.java
@@ -0,0 +1,13 @@
+package logica;
+
+import igu.Pantalla;
+
+public class Calculadora_IMC {
+
+    public static void main(String[] args) {
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+        
+    }
+}
